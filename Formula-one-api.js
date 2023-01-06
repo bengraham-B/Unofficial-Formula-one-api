2b1c6a0369ecb1b46cd26c36aff47cbae42ec0cb
Ben Graham <bengraham@Bens-MacBook-Air.local>
2023-01-06 17:16:42 +0200

    Formula one API 🏎

---
+++ Formula-one-api.js
@@ -0,0 +1,29 @@
+export default [
+    {
+        "teamName": "Redbull Racing Oracle",
+        "teamPrincipal": {
+            "name": "Christian Horner",
+            "country": "Great Britain"
+        },
+        "drivers" : {
+            "no.1": {
+                "name":"Max Verstappen",
+                "RaceNumber": "1",
+                "country": "",
+                "wins": ""
+            },
+            "no.2" : {
+                "name":"Sergio Perez",
+                "RaceNumber": "11",
+                "country": "Mexico",
+                "wins": ""
+            },
+            "no.3" : {
+                "name":"Daniel Ricardo",
+                "RaceNumber": "3",
+                "country": "Austrilia",
+                "wins": ""
+            }
+        }
+    }
+]
