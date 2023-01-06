d55ebffc176b08cceb014a8453ff22568409671a
Ben Graham <bengraham@Bens-MacBook-Air.local>
2023-01-06 22:01:15 +0200

    I prefer the API in a js format 🦀

---
+++ Formula-one-api.js
@@ -0,0 +1,79 @@
+export default [
+    {
+        "TeamName": "Redbull Racing Oracle",
+        "Headquaters": "",
+        "CountryOfOrgin": "",
+        "RaceHistory": {
+            "TotalWins": "92",
+            "TeamsPodiums": "234",
+            "TeamPoints": "6388",
+            "TeamFastestLaps": "84",
+            "TeamPolePositions": "81",
+            "TotalCounstructorChampionships":"5",
+            "CounstructorChampionshipsList": {
+                "1": "2010",
+                "2": "2011",
+                "3": "2012",
+                "4": "2013",
+                "5": "2022"
+            },
+        },
+        "TeamPrincipal": {
+            "Name": "Christian Horner",
+            "CountryOfBirth": "Great Britain"
+        },
+        "Car":{
+            "Chasis": {},
+            "Tires": "Pirelli",
+            "Engin": {
+                "2007": "Renault",
+                "2008": "Renault",
+                "2009": "Renault",
+                "2010": "Renault",
+                "2011": "Renault",
+                "2012": "Renault",
+                "2013": "Renault",
+                "2014": "Renault",
+                "2015": "Renault",
+                "2016": "TAG Heuer-badged Renault engines",
+                "2017": "TAG Heuer-badged Renault engines",
+                "2018": "TAG Heuer-badged Renault engines",
+                "2019": "Honda",
+                "2020": "Honda",
+                "2021": "Honda",
+                "2022": "Redbull Power Trains"
+            }
+
+        },
+        "Drivers" : {
+            "No.1": {
+                "Name":"Max Verstappen",
+                "RaceNumber": "1",
+                "CountryOfBirth": "Belgium",
+                "RaceHistory": {
+                    "WorldChampionships": {
+                        "first": "2021",
+                        "second": "2022"
+                    },
+                    "TotalPoints": "",
+                    "TotalPodiums":"",
+                    "TotalFastestLaps": "",
+                    "TotalPolePositions": "",
+                    "TotalWins": ""
+                }
+            },
+            "no.2" : {
+                "Name":"Sergio Perez",
+                "RaceNumber": "11",
+                "Country": "Mexico",
+                "Wins": ""
+            },
+            "no.3" : {
+                "Name":"Daniel Ricardo",
+                "RaceNumber": "3",
+                "Country": "Austrilia",
+                "Wins": ""
+            }
+        }
+    }
+]
